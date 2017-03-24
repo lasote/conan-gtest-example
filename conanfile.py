5f8d7d243d0212a353518790f7f7545abc86402e
Uilian Ries <uilianries@gmail.com>
2017-03-24 11:09:33 -0300

    Using CMake helper
    - Change manual cmake commands to helpers
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,6 +9,8 @@ class ConanGTestExample(ConanFile):
     name = "conan-gtest-example"
     version = "0.1.0"
     url = "https://github.com/uilianries/conan-gtest-example"
+    author = "lasote"
+    license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
     generators = "cmake"
     exports = "*"
@@ -18,18 +20,18 @@ class ConanGTestExample(ConanFile):
     default_options = "gtest:shared=True", "shared=False"
 
     def build(self):
+        shared = {"BUILD_SHARED_LIBS": self.options.shared}
         cmake = CMake(self.settings)
-        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else "-DBUILD_SHARED_LIBS=OFF"
-        self.run('cmake %s %s %s' % (self.conanfile_directory, cmake.command_line, shared))
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.configure(self, defs=shared)
+        cmake.build(self)
 
     def package(self):
         self.copy("*.h", dst="include")
         self.copy("*.lib", dst="lib", src="lib", keep_path=False)
         self.copy("*.dll", dst="bin", src="bin", keep_path=False)
+        self.copy("*.dylib", dst="bin", src="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ["encrypter"]
-
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,23 +1,26 @@
 import os
+import sys
 from conans.model.conan_file import ConanFile
 from conans import CMake
-
-
-USERNAME = os.getenv("CONAN_USERNAME", "lasote")
-CHANNEL = os.getenv("CONAN_CHANNEL", "testing")
 
 
 class TestConanGTestExample(ConanFile):
     name = "TestConanGTestExample"
     version = "0.1.0"
+    author = "lasote"
+    url = "https://github.com/lasote/conan-gtest-example"
+    license = "MIT"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "conan-gtest-example/0.1.0@%s/%s" % (USERNAME, CHANNEL)
+    user = os.getenv("CONAN_USERNAME", "lasote")
+    channel = os.getenv("CONAN_CHANNEL", "testing")
+    requires = "conan-gtest-example/0.1.0@%s/%s" % (user, channel)
+    cmake = None
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+        self.cmake = CMake(self.settings)
+        self.cmake.configure(self, source_dir=self.conanfile_directory, build_dir="./")
+        self.cmake.build(self)
 
     def imports(self):
         self.copy("*.so", "bin", "lib")
@@ -25,6 +28,5 @@ class TestConanGTestExample(ConanFile):
         self.copy("*.dylib", "bin", "bin")
 
     def test(self):
-        target = "RUN_TESTS" if self.settings.os == "Windows" else "test"
-        self.run("cmake --build . --target %s" % target)
-
+        target_test = "RUN_TESTS" if self.settings.os == "Windows" else "test"
+        self.cmake.build(self, target=target_test)
